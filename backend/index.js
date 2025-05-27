0e555ed65700251d7aa2a4252f71bc3009eb3e84
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-27 21:49:54 +0500

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get('/test',(req, res) => {
+    res.status(200).send('Test endpoint is working!');
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
