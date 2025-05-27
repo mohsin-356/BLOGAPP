a65e30e839e2c80e244b3a3b6c42b307979502a3
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-27 23:50:09 +0500

    initializing basic routes:[POST, USER, COMMENT]

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import userRouter from './routes/user.route.js';
 import postRouter from './routes/post.route.js';
 import commentRouter from './routes/comment.route.js';
+import mongoose from 'mongoose';
 import connectDB from './lib/connectDB.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
--- backend/lib/connectDB.js
+++ backend/lib/connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
-async function connectDB() {
+const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_CONNECT);
+        await mongoose.connect('mongodb://127.0.0.1:27017/blogapp');
         console.log('✅ Connected to database successfully');
     } catch (err) {
         console.error('❌ Database connection error:', err);
