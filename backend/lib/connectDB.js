27da57380f2414dafa649a0bb0e9ff6c10c3ac77
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-27 23:18:23 +0500

    initializing basic routes:[POST, USER, COMMENT]

---
+++ backend/lib/connectDB.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(process.env.DB_CONNECT);
+        console.log('✅ Connected to database successfully');
+    } catch (err) {
+        console.error('❌ Database connection error:', err);
+    }
+}
+
+export default connectToDatabase;
