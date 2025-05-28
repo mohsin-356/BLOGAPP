0321993ef4d4eda8cf1446f9450395252ade1df3
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-28 12:14:28 +0500

    initializing basic routes:[POST, USER, COMMENT]

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,8 +1,38 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
 
-const postSchema = new Schema(
-  { timestamps: true }
-);
+const postSchema = new Schema({
+  img:{
+    type: String,
+  },
+  title:{
+    type:String,
+    required: true,
+    trim: true,
+  },
+  slug:{
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  desc:{
+    type: String,
+    trim: true,
+  },
+  content:{
+    type: String,
+    required: true,
+  },
+  isFeatured:{
+    type:Boolean,
+    default:false 
+   },
+   visit:{
+    type:Boolean,
+    default:0
+   }
+},
+{ timestamps: true });
 
 export default mongoose.model("Post", postSchema);
