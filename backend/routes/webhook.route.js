d8ea8921cd649866c8112eaa1bd5029d457dff65
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-29 15:54:27 +0500

    add body-parser dependency and clean up webhook route code

--- backend/routes/webhook.route.js
+++ backend/routes/webhook.route.js
@@ -1,9 +1,3 @@
-// import express from 'express';
-// import { clerkWebHook } from '../controllers/webhook.controller';
-// const router = express.Router();
-// router.post('/clerk',clerkWebHook); 
-// export default router;
-
 import express from 'express';
 import { clerkWebHook } from '../controllers/webhook.controller.js'; // ✅ Added .js extension
 
