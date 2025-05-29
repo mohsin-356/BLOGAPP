19a424077f6960e58e20c2653b2b125436cf298b
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-29 15:57:14 +0500

    add body-parser middleware to handle raw JSON in clerk webhook route

--- backend/routes/webhook.route.js
+++ backend/routes/webhook.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { clerkWebHook } from '../controllers/webhook.controller.js'; // ✅ Added .js extension
+import bodyParser from 'body-parser';
 
 const router = express.Router();
 
-router.post('/clerk', clerkWebHook); 
+router.post('/clerk', bodyParser.raw({ type: 'application/json' }),clerkWebHook); 
 
 export default router;
