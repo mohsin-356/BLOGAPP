84a579a300f72c95ba7342a18c94b8352adf907e
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-31 21:18:49 +0500

    add requireAuth middleware to protect routes and clean up auth-state endpoint

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import userRouter from './routes/user.route.js';
 import postRouter from './routes/post.route.js';
 import commentRouter from './routes/comment.route.js';
 import webhookRouter from './routes/webhook.route.js';
-import {clerkMiddleware} from '@clerk/express';
+import {clerkMiddleware, requireAuth} from '@clerk/express';
 import connectDB from './lib/connectDB.js';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -17,14 +17,14 @@ app.use('/posts', postRouter);
 app.use('/comments', commentRouter);
 app.use('/webhooks', webhookRouter);
 
-app.use('/auth-state', (req, res) => {
-  // console.log(req.auth);
-  const authState = req.auth();
-res.json(authState);
-});
+// app.use('/auth-state', (req, res) => {
+//   // console.log(req.auth());
+//   const authState = req.auth();
+// res.json(authState);
+// });
 
-app.use('/protect',(req,res)=>{
-  const {userId}=req.auth();
+app.use('/protect', requireAuth(), (req, res) => {
+  const { userId } = req.auth();
   if (!userId) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,6 +3,11 @@ import { Schema } from "mongoose";
 
 const userSchema = new Schema(
   {
+    clerkUserId:{
+      type:String,
+      required: true,
+      unique: true
+    },
     username: {
       type: String,
       required: true,
